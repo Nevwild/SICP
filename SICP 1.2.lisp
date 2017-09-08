d47d26ca45a88a16050782bffe136bb56bb0e6b6
Nevill Wilder <nevillwilder@gmail.com>
2017-09-07 17:14:28 -0700

    Added additional solutions

--- SICP 1.2.lisp
+++ SICP 1.2.lisp
@@ -1,36 +1,35 @@
-MIT/GNU Scheme running under OS X
+;Exercise 1.2
 
-Copyright (C) 2014 Massachusetts Institute of Technology
-This is free software; see the source for copying conditions. There is NO
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;There are many ways to present this in prefix notation. I find the following ways immediately apparent. 
 
-Image saved on Saturday May 17, 2014 at 2:39:25 AM
-  Release 9.2 || Microcode 15.3 || Runtime 15.7 || SF 4.41 || LIAR/x86-64 4.118
-  Edwin 3.116
+;normal prefix
+ (/(+ 5	4 (- 2(- 3(+ 6(/ 4 5)))))(* 3(- 6 2)(- 2 7)))
 
-1 ]=> (/(+ 5 4))
+;pretty print two levels deep
+ (/(+ 	5 
+		4
+		(- 2(- 3(+ 6(/ 4 5)))))
+	(* 	3
+		(- 6 2)
+		(- 2 7)
+	 )
+   )
 
-;Value: 1/9
+;pretty print the whole way down
+ (/(+ 	5 
+		4
+		(- 2
+		   (- 3
+			  (+ 6
+				 (/ 4
+					5)))))
+	(* 	3
+		(- 6 
+		   2)
+		(- 2 
+		   7)
+	 )
+   )
 
-1 ]=> (/(+ 
-			5 
-			4
-			(- 2(- 3(+ 6(/ 4 5)))))
-		
-)
 
-;Value: 5/74
 
-1 ]=> (/(+ 
-			5 
-			4
-			(- 2(- 3(+ 6(/ 4 5)))))
-		(* 
-			3
-			(- 6 2)
-			(- 2 7))
-	)
-
-;Value: -37/150
-
-1 ]=> 
