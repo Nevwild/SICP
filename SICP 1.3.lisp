b7966c78235be4eea7de0ac1e79c27a377c3ce60
Nevill Wilder <nevillwilder@gmail.com>
2017-09-07 17:40:03 -0700

    Update SICP 1.3.lisp

--- SICP 1.3.lisp
+++ SICP 1.3.lisp
@@ -1,35 +1,25 @@
-MIT/GNU Scheme running under OS X
+;Exercise 1.3
 
-Copyright (C) 2014 Massachusetts Institute of Technology
-This is free software; see the source for copying conditions. There is NO
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-Image saved on Saturday May 17, 2014 at 2:39:25 AM
-  Release 9.2 || Microcode 15.3 || Runtime 15.7 || SF 4.41 || LIAR/x86-64 4.118
-  Edwin 3.116
 
-1 ]=> (define (square x) (* x x))
+(define (square x) (* x x))
 
-;Value: square
+(define (sum-of-squares x y)
+ 	(+ (square x) (square y)))
 
-1 ]=> (define (sum-of-squares x y)
-  		(+ (square x) (square y)))
+(define (min x y)
+ 	(if  (< x y)
+		x
+		y))
 
-;Value: sum-of-squares
+(define (sum-of-largest-squares x y z)
+		(if (= x(min x(min y z)))
+			(sum-of-squares y z)
+			(sum-of-largest-squares y z x)))
 
-1 ]=> (define (min x y)
- 		(if  (< x y)
-			x
-			y))
 
-;Value: min
 
-1 ]=> (define (sum-of-largest-squares x y z)
-			(if (= x(min x(min y z)))
-				(sum-of-squares y z)
-				(sum-of-largest-squares y z x)))
-
-;Value: sum-of-largest-squares
+; MARK: Tests as run in Edwin
 
 1 ]=> (sum-of-largest-squares 2 3 1)
 
@@ -39,8 +29,6 @@ Image saved on Saturday May 17, 2014 at 2:39:25 AM
 
 ;Value: 13
 
-1 ]=> (sum-of-largest-squares 1 2 3)
+(sum-of-largest-squares 1 2 3)
 
 ;Value: 13
-
-1 ]=> 
