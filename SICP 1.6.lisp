2773007d382691d3d676312995c2fe5f8dbb8350
Nevill Wilder <nevillwilder@gmail.com>
2017-09-24 19:00:47 -0700

    1.6 Repl Output

--- SICP 1.6.lisp
+++ SICP 1.6.lisp
@@ -1,3 +1,5 @@
+;“Every reader should ask himself periodically “Toward what end, toward what end?”—but do not ask it too often lest you pass up the fun of programming for the constipation of bittersweet philosophy.”
+
 ;Exercise 1.6
 ;Setup from text:
 
@@ -21,7 +23,7 @@
 
 
 
-;Exercise 1.6: Alyssa P. Hacker doesn’t see why if needs to be provided as a special form. “Why can’t I just define it as an ordinary procedure in terms of cond?” she asks. Alyssa’s friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:
+;Alyssa P. Hacker doesn’t see why if needs to be provided as a special form. “Why can’t I just define it as an ordinary procedure in terms of cond?” she asks. Alyssa’s friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:
 
 (define (new-if predicate 
                 then-clause 
@@ -40,7 +42,82 @@
 
 (define (sqrt-iter guess x)
   (new-if (good-enough? guess x)
-          (display guess) guess
+          guess
           (sqrt-iter (improve guess x) x)))
 
 ;What happens when Alyssa attempts to use this to compute square roots? Explain.”
+
+
+;Value: sqrt-iter
+
+1 ]=> 
+;Value: average
+
+1 ]=> 
+;Value: improve
+
+1 ]=> 
+;Value: good-enough?
+
+1 ]=> 
+;Value: sqrt
+
+1 ]=> 
+;Value: new-if
+
+1 ]=> 
+;Value: 5
+
+1 ]=> 
+;Value: 5
+
+1 ]=> 
+;Value: 0
+
+1 ]=> 
+;Value: 0
+
+1 ]=> 
+;Value: sqrt-iter
+
+1 ]=> (sqrt 124)
+
+;Aborting!: maximum recursion depth exceeded
+
+1 ]=> (define (sqrt-iter guess x)
+	(if (good-enough? guess x)
+		guess
+		(sqrt-iter (improve guess x) 
+ 	x)))
+
+;Value: sqrt-iter
+
+1 ]=> (sqrt 124)
+
+;Value: 11.135528727830582
+
+1 ]=> (define (sqrt-iter guess x)
+	(new-if (good-enough? guess x)
+		guess
+		(sqrt-iter (improve guess x) 
+ 	x)))
+
+;Value: sqrt-iter
+
+1 ]=> (sqrt 124)
+
+;Aborting!: maximum recursion depth exceeded
+
+1 ]=> (sqrt 12)
+
+;Aborting!: maximum recursion depth exceeded
+
+1 ]=> 9
+
+;Value: 9
+
+1 ]=> (sqrt 9)
+
+;Aborting!: maximum recursion depth exceeded
+
+1 ]=> 
